7195650876eec17186de5cfc8cfa141650420211
denkoroku <cathriddoch@gmail.com>
2019-10-28 18:35:02 +0000

    basic functions

---
+++ src/components/Film.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Film extends Component {
+  render(){
+    return (
+      <div>
+        <h4>{this.props.url}</h4>
+        <p>{this.props.name}</p>
+
+      </div>
+    );
+  }
+}
+
+export default Film;
---
+++ src/components/FilmList.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Film from "./Film";
+
+class FilmList extends Component{
+  render(){
+    const filmNodes = this.props.films.map(film => {
+      return (
+        <Film name={ film.name } key={ film.id } url={film.url}>
+        </Film>
+      );
+    });
+    return (
+      <div className="film-list">
+        { filmNodes }
+
+      </div>
+    );
+}
+}
+
+
+
+export default FilmList;
